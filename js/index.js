5389184925ef2f35573eb4882c583973b36e71ea
Isaac Bell <ibell@athn.org>
2019-12-09 17:59:21 -0600

    Documenting progress

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/* If you're quite good at javascript, I am 
+ * certainly NOT, then you may want to 
+ * look at Zach Saucier's article 
+ * "Controlling CSS Animations and Transitions with JavaScript"
+ * at https://css-tricks.com/controlling-css-animations-transitions-javascript/
+ * to enable the user to pause the animation.
+ * Scroll down to the section on
+ * Manipulating CSS Animations
+ */
